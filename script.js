f2a50e1fe6c31a1a2933d46a2f5bee962f207a6d
yago-Wes <wesleywwll@gmail.com>
2024-01-10 11:16:09 -0300

    add reset e animeção botão - e +

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
-let content_html  = [document.getElementById('plu'),document.getElementById('less')]
+const content_html = [document.getElementById('plu'), document.getElementById('less')]
+const content_contador = [document.getElementById('acc'), document.getElementById('reset')]
 
-const content_contador = document.getElementById('acc')
+content_html[0].addEventListener('click', () => {
+    let Number_contador = parseInt(content_contador[0].innerHTML)
+    Number_contador++
+    content_contador[0].innerHTML = Number_contador
+    content_html[0].style.width = '45px'
+    content_html[0].style.height = '45px'
+    setTimeout(() => {
+        content_html[0].style.width = '50px'
+        content_html[0].style.height = '50px'
+    }, 100)
 
-content_html[0].addEventListener('click',()=>{
-  let Number_contador = parseInt(content_contador.innerHTML)
-  Number_contador++
-  content_contador.innerHTML = Number_contador
 })
 
-content_html[1].addEventListener('click',()=>{
-    let Number_contador = parseInt(content_contador.innerHTML)
+content_html[1].addEventListener('click', () => {
+    let Number_contador = parseInt(content_contador[0].innerHTML)
     Number_contador--
-    content_contador.innerHTML = Number_contador
-  })
-  
+    content_contador[0].innerHTML = Number_contador
+    content_html[1].style.width = '45px'
+    content_html[1].style.height = '45px'
+    setTimeout(() => {
+        content_html[1].style.width = '50px'
+        content_html[1].style.height = '50px'
+    }, 100)
+
+})
+
+content_contador[0].addEventListener('mouseover', () => {
+
+    content_contador[0].style.display = 'none'
+    content_contador[1].style.display = 'flex'
+})
+content_contador[1].addEventListener('mouseout', () => {
+
+    content_contador[0].style.display = 'flex'
+    content_contador[1].style.display = 'none'
+
+
+})
+
+content_contador[1].addEventListener('click', () => {
+
+    content_contador[0].innerHTML = 0
+})
